adfa73fb2018e06f14c72d75a0d1e5e12bb3ab7c
EC2 Default User <ec2-user@ip-172-31-1-192.ap-southeast-2.compute.internal>
2025-05-21 13:20:42 +0000

    [Update]topレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,9 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    if book.save
       redirect_to books_path
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'homes/top'
   get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
-  root to: "homes#top"
+  root to: "homes#top", as: "top"
 
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update]
